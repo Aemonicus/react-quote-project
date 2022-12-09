56b0964bbc780b36163c9d3e1b45ba58311a2d2a
Florent VICENTE <vicentegroupe@gmail.com>
2022-12-09 12:41:14 +0100

    routes, redirect, params and nested routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+import { NewQuote } from "./pages/NewQuote";
+import { QuoteDetail } from "./pages/QuoteDetail";
+import { Quotes } from "./pages/Quotes";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route exact path="/"><Redirect to="/quotes" /></Route>
+      <Route exact path="/quotes"><Quotes /></Route>
+      <Route path="/quotes/:quoteId"><QuoteDetail /></Route>
+      <Route path="/new-quote"><NewQuote /></Route>
+    </Switch>
   );
 }
 
